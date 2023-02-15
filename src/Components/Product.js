aa9f22b130196f42e7739449eea18821f6b0c024
Dhia Bejaoui <bejaoui.dhia@esprit.tn>
2023-02-15 17:01:27 +0100

    atelier 2 part 1

--- src/Components/Product.js
+++ src/Components/Product.js
@@ -1,30 +1,50 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from 'react';
+import '../css/Product.css';
 
-export class Product extends Component {
-  render() {
-    const { product } = this.props;
-    return (
-      <>
-        <div className="card" style={{ width: "18rem" }}>
-          <img
-            src={require(`../assets/images/${product.img}`)}
-            className="card-img-top"
-            alt="..."
-          />
-          <div className="card-body">
-            <h5 className="card-title">{product.name}</h5>
-            <p className="card-text">{product.description}</p>
-            <p className="card-text">{product.price} DT</p>
-            <p className="card-text">Quantity: {product.quantity}</p>
-            <p className="card-text">Likes: {product.like}</p>
-            <a href="#" className="btn btn-primary">
-              Like
-            </a>
-          </div>
+const Product = ({ product, buyProduct }) => {
+  const [likes, setlikes] = useState(0);
+  const handleLikes = () => {
+    setlikes(likes + 1);
+    console.log(likes);
+    //console.log(product.like);
+  };
+  useEffect(() => {
+    product.like = likes;
+  }, [product, likes]);
+
+  return (
+    <div
+      className={product.like > 5 ? 'card me-5 bestProduct' : 'card me-5'}
+      style={{ width: '18rem' }}>
+      <img
+        src={require(`../assets/images/${product.img}`)}
+        className='card-img-top'
+        alt='...'
+      />
+      <div className='card-body'>
+        <h5 className='card-title'>{product.name}</h5>
+        <p className='card-text'>{product.description}</p>
+        <p className='card-text'>{product.price} DT</p>
+        <p className='card-text'>Quantity: {product.quantity}</p>
+        <p className='card-text'>Likes: {product.like}</p>
+        <div className='d-flex justify-content-between'>
+          <button
+            className='btn btn-primary'
+            onClick={() => {
+              handleLikes();
+            }}>
+            Like
+          </button>
+          <button
+            className='btn btn-warning'
+            disabled={product.quantity === 0}
+            onClick={buyProduct}>
+            Buy
+          </button>
         </div>
-      </>
-    );
-  }
-}
+      </div>
+    </div>
+  );
+};
 
 export default Product;
--- src/Components/Products.js
+++ src/Components/Products.js
@@ -1,22 +1,59 @@
-import React, { Component } from "react";
-import Product from "./Product";
-import data from "../data/products.json";
+import React, { Component } from 'react';
+import Product from './Product';
+import data from '../data/products.json';
+import '../css/Products.css';
 
 export class Products extends Component {
   // eslint-disable-next-line no-useless-constructor
   constructor() {
     super();
-    this.state = { data: data };
+    this.state = { data: data, message: '', show: false };
   }
   componentDidMount() {
     console.log(this.state.data);
+    setTimeout(() => {
+      this.setState({ show: true });
+    }, '3000');
   }
+
+  buyProduct = () => {
+    this.setState({ message: 'You bought an item' });
+    console.log(this.state.message);
+  };
+
   render() {
     return (
-      <div>
-        {this.state.data.map((product) => (
-          <Product product={product} />
-        ))}
+      <div className='container mt-5 '>
+        {this.state.show && (
+          <div className='alert alert-success' role='alert'>
+            <h4 className='alert-heading'>Welcome!</h4>
+            <p>
+              Thank you for choosing our store, we hope you enjoy your shopping
+              experience !
+            </p>
+          </div>
+        )}
+        <div className='wrap mb-3'>
+          {this.state.data.map((product, index) => (
+            <Product
+              product={product}
+              key={index}
+              buyProduct={this.buyProduct}
+            />
+          ))}
+        </div>
+        {this.state.message && (
+          <div
+            className='alert alert-warning alert-dismissible fade show'
+            role='alert'>
+            <strong>Success!</strong> {this.state.message}
+            <button
+              type='button'
+              className='btn-close'
+              data-bs-dismiss='alert'
+              aria-label='Close'></button>
+          </div>
+        )}
       </div>
     );
   }
